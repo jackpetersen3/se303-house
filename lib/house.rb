51c1357b324add74a64cb3f76f4d691fb135f536
Jack Petersen <jack.petersen3@gmail.com>
2022-04-26 11:16:55 -0700

    create new method for horse_and_hound creating this method allowed me to delete cases 11 and 12 in the case statement and replace with else

--- lib/house.rb
+++ lib/house.rb
@@ -22,14 +22,19 @@ class House
             "This is the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
         when 10
             "This is the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
-        when 11
-            "This is the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
-        when 12
-            "This is the horse and the hound and the horn that belonged to the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
+        else
+            "This is the#{horse_and_hound(number)} farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
         end
-        
     end
     
+    def horse_and_hound(number)
+        if number == 12
+            " horse and the hound and the horn that belonged to the" 
+        else  
+            ""
+      end
+    end
+
     def recite
         (1..12).each.collect { |number| line(number) }.join("\n")   
     end
