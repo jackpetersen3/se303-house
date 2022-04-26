f15378d1a9484b8d43fcc17f83479ca942315483
Jack Petersen <jack.petersen3@gmail.com>
2022-04-26 11:23:06 -0700

    Added farmer method, deleted case 10

--- lib/house.rb
+++ lib/house.rb
@@ -20,20 +20,25 @@ class House
             "This is the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
         when 9    
             "This is the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
-        when 10
-            "This is the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
         else
-            "This is the#{horse_and_hound(number)} farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
+            "This is the#{horse_and_hound(number)}#{farmer(number)} rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
         end
     end
     
     def horse_and_hound(number)
-        if number == 12
+        if number >= 12
             " horse and the hound and the horn that belonged to the" 
         else  
             ""
       end
     end
+
+    def farmer(number)
+        if number >= 11
+            " farmer sowing his corn that kept the"
+        end
+    end 
+
 
     def recite
         (1..12).each.collect { |number| line(number) }.join("\n")   
