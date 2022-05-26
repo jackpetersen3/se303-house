e4046b65dbf85bf38ffd0f497e588b86f328197e
Jack Petersen <jack.petersen3@gmail.com>
2022-05-26 16:17:09 -0700

    add phrases array with all of the lines of the song

--- lib/house.rb
+++ lib/house.rb
@@ -1,7 +1,21 @@
 class House
+    PHRASES = [
+        " the malt that lay in",
+        " the rat that ate",
+         " the rat that ate",
+        " the cat that killed",
+        " the dog that worried",
+        " the cow with the crumpled horn that tossed",
+        " the maiden all forlorn that milked",
+        " the man all tattered and torn that kissed",
+        " the priest all shaven and shorn that married",
+        " the rooster that crowed in the morn that woke",
+        " the farmer sowing his corn that kept",
+        " the horse and the hound and the horn that belonged to" 
+    ]
 
     def line(number)
-        "This is#{horse(number)}#{farmer(number)}#{rooster(number)}#{priest(number)}#{man(number)}#{maiden(number)}#{cow(number)}#{dog(number)}#{cat(number)}#{rat(number)}#{malt(number)} the house that Jack built.\n"
+        "This is#{phrase(number)}#{farmer(number)}#{rooster(number)}#{priest(number)}#{man(number)}#{maiden(number)}#{cow(number)}#{dog(number)}#{cat(number)}#{rat(number)}#{malt(number)} the house that Jack built.\n"
     end
     
     def horse(number)
@@ -69,25 +83,7 @@ class House
         end
     end
 
-    def phrases
-        [
-            " the malt that lay in",
-            " the rat that ate",
-            " the rat that ate",
-            " the cat that killed",
-            " the dog that worried",
-            " the cow with the crumpled horn that tossed",
-            " the maiden all forlorn that milked",
-            " the man all tattered and torn that kissed",
-            " the priest all shaven and shorn that married",
-            " the rooster that crowed in the morn that woke",
-            " the farmer sowing his corn that kept",
-            " the horse and the hound and the horn that belonged to" 
-        ]
-    end
-
     def phrase(number)
-        phrases(number - 1)
     end
 
     def recite
