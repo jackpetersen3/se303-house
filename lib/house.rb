c95962dd85f164502ca7d35a33a84c4a1ab5a7f8
Jack Petersen <jack.petersen3@gmail.com>
2022-05-30 12:09:51 -0700

    Fill the verb and subject arrays with their respective pieces of the phrases

--- lib/house.rb
+++ lib/house.rb
@@ -16,9 +16,35 @@ class House
     end
 
     def subject
+        [
+            " the malt ",
+            " the rat ",
+            " the cat ",
+            " the dog ",
+            " the cow with the crumpled horn ",
+            " the maiden all forlorn ",
+            " the man all tattered and torn ",
+            " the priest all shaven and shorn ",
+            " the rooster that crowed in the morn ",
+            " the farmer sowing his corn ",
+            " the horse and the hound and the horn " 
+        ]
     end
 
     def verb
+        [
+            "that lay in",
+            "that ate",
+            "that killed",
+            "that worried",
+            "that tossed",
+            "that milked",
+            "that kissed",
+            "that married",
+            "that woke",
+            "that kept",
+            "that belonged to" 
+        ]
     end
 
     def initialize(beginning = 'This is')
